9e58d0e668fd6951720a6ccaa71d9d63426825aa
Jan Bölsche <regular.gonzales@googlemail.com>
2019-09-26 19:14:24 +0200

    Readd ws.getAddress

--- inject.js
+++ inject.js
@@ -1,10 +1,13 @@
 var WS = require('multiserver/plugins/ws')
+const {encode, decode} = require('multiserver-address')
 
 module.exports = function (createHandlers) {
   var exports = {}
   exports.name = 'ws'
   exports.version = require('./package.json').version
-  exports.manifest = {}
+  exports.manifest = {
+    getAddress: 'sync'
+  }
 
   exports.init = function (sbot, config) {
     var port
@@ -33,6 +36,17 @@ module.exports = function (createHandlers) {
     })
 
     return {
+      getAddress: function() {
+        const scopes = ['device', 'private', 'public']  // TODO: get from multiserver-scopes
+        const wsAddresses = scopes.reduce( (acc, scope) => {
+          const addresses = decode(sbot.getAddress(scope))
+          acc = acc.concat(addresses.find(stack=>{
+            return stack[0].name == 'ws'
+          }))
+          return acc
+        }, [])
+        return encode(wsAddresses)
+      },
       use: function (handler) {
         if(handlers.layers) handlers.layers.push(handler)
       }
